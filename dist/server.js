871d36317cdaf9a9f1093339c71ff9199219b2b5
rycsouza <rychard.souza1111@gmail.com>
2024-08-27 16:27:18 -0400

    configs vercel

---
+++ dist/server.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const dotenv_1 = __importDefault(require("dotenv"));
+const ENV = dotenv_1.default.config().parsed;
+class App {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.http = http_1.default.createServer(this.app);
+        this.io = new socket_io_1.Server(this.http);
+        this.listenSocket();
+        this.setupRoutes();
+    }
+    listenServer() {
+        this.http.listen(ENV.PORT, () => console.log('Servidor Rodando => http://localhost:3000/chat'));
+    }
+    listenSocket() {
+        this.io.on('connection', (socket) => {
+            console.log(socket.id);
+            socket.on('register', (user) => {
+                socket.userId = user;
+                console.log(`Usuário Registrado: ${user}`);
+            });
+            socket.on('message', (msg) => {
+                this.io.emit('message', (`${socket.userId}: ${msg}`));
+            });
+        });
+    }
+    setupRoutes() {
+        this.app.get('/chat', (req, res) => {
+            res.sendFile(`${__dirname}/index.html`);
+        });
+    }
+}
+const app = new App();
+app.listenServer();
+//# sourceMappingURL=server.js.map
